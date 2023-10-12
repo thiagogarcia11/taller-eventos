f8b7a2f470f9f1085db26822e8498232dbfdeee0
thiagogarcia11 <113443907+thiagogarcia11@users.noreply.github.com>
2023-10-12 10:38:19 -0300

    Add files via upload

--- eventos.js
+++ eventos.js
@@ -3,6 +3,8 @@ function saludar() {
 }
 
 const div = document.querySelector("div");
-div.addEventListener("click", function () {
-  alert("Hola! Soy el div");
+div.addEventListener("click", (e) => {
+  if (e.target !== document.querySelector("button")) {
+    alert("Hola! Soy el div");
+  }
 });
